844301ee273c2edefa11e44e48114c8f27ec042f
seraphJ <j.anderson822@gmail.com>
2023-12-28 15:00:22 -0500

    Updated

--- web_app/functions.py
+++ web_app/functions.py
@@ -1,13 +1,14 @@
-FILEPATH = "todos.txt"
+import os
+ 
+ 
+def get_todos():
+    filepath = os.path.join(os.getcwd(), "todos.txt")
+    with open(filepath, "r") as file_local:
+        todos = [line.strip() for line in file_local.readlines()]
+    return todos
 
-def get_todos(filepath=FILEPATH):
-    """ Get the to-do item list from the text file."""
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
 
-
-def write_todos(todos_arg, filepath=FILEPATH):
+def write_todos(todos_arg, filepath=os.path.join(os.getcwd(), "todos.txt")):
     """ Write the to-do item list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
