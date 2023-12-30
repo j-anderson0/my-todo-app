6d908b6a8961992c9c4db3d41aa2f8a8fad87d7d
seraphJ <j.anderson822@gmail.com>
2023-12-30 11:18:24 -0500

    Updated to better gpt format

--- web_app/functions.py
+++ web_app/functions.py
@@ -1,14 +1,27 @@
-import os
- 
- 
-def get_todos():
-    filepath = os.path.join(os.getcwd(), "todos.txt")
-    with open(filepath, "r") as file_local:
-        todos = [line.strip() for line in file_local.readlines()]
-    return todos
+FILEPATH = "todos.txt"
 
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the list of to-do items."""
+    try:
+        with open(filepath, 'r') as file_local:
+            todos_local = file_local.readlines()
+    except FileNotFoundError:
+        # If the file doesn't exist, create it and return an empty list
+        todos_local = []
+        with open(filepath, 'w') as file_local:
+            pass
+    return todos_local
 
-def write_todos(todos_arg, filepath=os.path.join(os.getcwd(), "todos.txt")):
-    """ Write the to-do item list in the text file."""
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do item list to the text file."""
     with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+        # Ensure each todo item ends with a newline character
+        for todo in todos_arg:
+            if not todo.endswith('\n'):
+                todo += '\n'
+            file.write(todo)
+
+# Example usage
+# todos = get_todos()
+# todos.append('New Todo Item\n')
+# write_todos(todos)
